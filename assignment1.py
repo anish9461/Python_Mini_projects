a42b7f7eda4ed7828411171c9252b8173a885434
anish9461 <anish9461@gmail.com>
2019-09-10 21:00:06 -0400

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,21 +1,13 @@
-class Test1:
-    t1text = "This is Test 1 class"
-    #List Comprehension for creating a list of cubes
-    cubes = []
-    cubes = [x**3 for x in range(10)]
-    def func1():
-        print("This is funtion 1 in class test 1")
-
-class Test2:
-    t2text = "This is Test 2 class"
-    #List Comprehension for creating a list of cubes
-    squares = []
-    squares = [x**2 for x in range(10)]
-    def func1():
-        f1text = "This is funtion 1 in class test 2"
+class HomeAppliances:
+    def __init__(self, productName, productCost):
+        self.productName = productName
+        self.productCost = productCost
+    
+    def displayProduct(self):
+        print(" "+ self.productName+"   ",self.productCost)
 
 
-class product:
+class ElectronicProducts:
     def __init__(self, productName, productCost):
         self.productName = productName
         self.productCost = productCost
@@ -26,42 +18,55 @@ class product:
 def getProductValue(dictproducts,product):
     return dictproducts[product].productCost        
 
+#function to display the exit message
+def exitMessage():
+    print(">----------------< Thankyou! For shopping Online on our portal >-------------------<")
+
 def main():
     print(">-------------< Welcome to online shopping portal >--------------<")
     
-    laptop = product("laptop",100)
-    phone = product("phone",50)
-    watch = product("watch",25)
+    laptop = ElectronicProducts("laptop",100)
+    phone = ElectronicProducts("phone",50)
+    watch = ElectronicProducts("watch",25)
 
-    listofProducts = [laptop,phone,watch]
+    television = HomeAppliances("television",1000)
+    microwave = HomeAppliances("microwave",500)
+    heater = HomeAppliances("heater",300)
+
+    listofProducts = [laptop,phone,watch,television,microwave,heater]
 
     #Dictionary Comprehension from the list
     dictionaryOfProducts = {f.productName:f for f in listofProducts}
-    print(dictionaryOfProducts)
     for i in listofProducts:
         i.displayProduct()
     cost = input("Enter a number to see items below that price: ")
     listOfUpdatedProducts = [item for item in listofProducts if item.productCost < int(cost)]
-    
     dictionaryOfUpdatedProducts = {listOfUpdatedProducts.index(f):f.productName for f in listOfUpdatedProducts}
-    
-    print(dictionaryOfUpdatedProducts[0])
-    
+    itemsInTheCart = []
     totalinCart = 0
     while(True):
         try:
             if dictionaryOfUpdatedProducts != {}:
-                print(dictionaryOfUpdatedProducts)
-                index = input("Select the index of the item to purchase:")
+                print("-----------------------------------------------")
+                print("Items Available : ",dictionaryOfUpdatedProducts)
+                print("-----------------------------------------------")
+                index = input("Select the index of the item to purchase : ")
                 totalinCart += getProductValue(dictionaryOfProducts,dictionaryOfUpdatedProducts[int(index)])
-                del dictionaryOfUpdatedProducts[int(index)]
-                print(totalinCart)
+                print("Selected Item : ",dictionaryOfUpdatedProducts[int(index)])
+                print(">--------------------------------------------<")
+                print("Total amount in the cart : ",totalinCart)
+                itemsInTheCart.append(dictionaryOfUpdatedProducts[int(index)])
+                print("Items in the cart : ",itemsInTheCart)
+                print(">--------------------------------------------<")
             else:
-                print("No items in this range")
+                print(">===============< Cart empty. Checking Out >===============<")
+                break
             
-        except KeyError:
-            print("index error")
-            
+        except Exception:
+            print("\nXXXXXXXXXXXXXXXXXXXXXXXX No such item for that index in the list XXXXXXXXXXXXXXXXXXXXXXXXXXX\n")
+            continue
+        del dictionaryOfUpdatedProducts[int(index)]
+    exitMessage()        
 
 if __name__ == "__main__":
     main()
