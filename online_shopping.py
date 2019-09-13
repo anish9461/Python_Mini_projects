5d52a52598a8beb2ffa066954f9156f6b3152526
anish9461 <anish9461@gmail.com>
2019-09-13 13:33:43 -0400

    updated application with some more products

--- online_shopping.py
+++ online_shopping.py
@@ -25,6 +25,24 @@ class HomeDecor:
     def displayProduct(self):
         print(" "+ self.productName+"   ",self.productCost)
 
+#Musical instruments class
+class MusicalInstruments:
+    def __init__(self, productName, productCost):
+        self.productName = productName
+        self.productCost = productCost
+    #display product function
+    def displayProduct(self):
+        print(" "+ self.productName+"   ",self.productCost)
+
+#Sports class
+class Sports:
+    def __init__(self, productName, productCost):
+        self.productName = productName
+        self.productCost = productCost
+    #display product function
+    def displayProduct(self):
+        print(" "+ self.productName+"   ",self.productCost)
+        
 #function to get the product value 
 def getProductValue(dictproducts,product):
     return dictproducts[product].productCost        
@@ -33,36 +51,66 @@ def getProductValue(dictproducts,product):
 def exitMessage(Cost,Cart):
     print("\n>----------------------------------------------------------------------------------<")
     print("Items purchased -> ",Cart)
-    print("Checkout Bill : ",Cost)
-    print(">----------------< Thankyou! For shopping Online on our portal >-------------------<")
+    print("Checkout Bill : $%i"%Cost)
+    print(">----------------< Thankyou! For shopping Online on our portal >-------------------<\n")
 
 #Main function
 def main():
     print(">-------------< Welcome to online shopping portal >--------------<\n")
-    print('>-------------< Electronic Products >-----------<')
+    
     laptop = ElectronicProducts("laptop",100)
     phone = ElectronicProducts("phone",50)
     watch = ElectronicProducts("watch",25)
-    print('>-------------< Home Appliances >---------------<')
+    
     television = HomeAppliances("television",1000)
     microwave = HomeAppliances("microwave",500)
     heater = HomeAppliances("heater",300)
-    print('>--------------< Home Decor >-------------------<')
     curtains = HomeDecor("curtains", 50)
     sheets = HomeDecor('sheets',25)
     pillows = HomeDecor('pillows',15)
+    guitar = MusicalInstruments("guitar", 125)
+    piano = MusicalInstruments('piano',100)
+    violin = MusicalInstruments('violin',75)
+    bat = Sports("bat", 75)
+    football = Sports('football',60)
+    diceGame = Sports('diceGame',25)
 
-    listofProducts = [laptop,phone,watch,television,microwave,heater, curtains, sheets, pillows]
+    listofProducts = [laptop,phone,watch,television,microwave,heater, curtains, sheets, pillows, guitar, piano, violin, bat, football, diceGame]
 
     #Dictionary Comprehension from the list
     dictionaryOfProducts = {f.productName:f for f in listofProducts}
     #for loop to display all the products
-    for i in listofProducts:
+    print('>-------------< Electronic Products >-----------<')
+    for i in listofProducts[0:3]:
         i.displayProduct()
-    cost = input("Enter a number to see items below that price: ")
-    #List comprehension for updated list of products
-    listOfUpdatedProducts = [item for item in listofProducts if item.productCost < int(cost)]
-    dictionaryOfUpdatedProducts = {listOfUpdatedProducts.index(f):f.productName for f in listOfUpdatedProducts}
+    print('>-------------< Home Appliances >---------------<')
+    for i in listofProducts[3:6]:
+        i.displayProduct()
+    print('>--------------< Home Decor >-------------------<')
+    for i in listofProducts[6:9]:
+        i.displayProduct()
+    print('>--------------< Musical Instruments >-------------------<')
+    for i in listofProducts[9:12]:
+        i.displayProduct()
+    print('>--------------< Sports >-------------------<')
+    for i in listofProducts[12:15]:
+        i.displayProduct()
+    while True:
+        
+        cost = input("Enter a number to see items below that price: ")
+        try:
+             #List comprehension for updated list of products
+            listOfUpdatedProducts = [item for item in listofProducts if item.productCost < int(cost)]
+            if listOfUpdatedProducts == []:
+                print(">----------< No items below that value >-------<")
+                print(">============< Re-enter the amount >===========<")
+                continue
+            dictionaryOfUpdatedProducts = {listOfUpdatedProducts.index(f):f.productName for f in listOfUpdatedProducts}
+            break
+        except Exception:
+            print("XXXXXXXXXXXXXXX Invalid Entry XXXXXXXXXXXXXXXX")
+   
+    
     itemsInTheCart = []
     totalinCart = 0
     #Infinite while loop to interact with the python program
@@ -79,13 +127,13 @@ def main():
                 totalinCart += getProductValue(dictionaryOfProducts,dictionaryOfUpdatedProducts[int(index)])
                 print("Selected Item : ",dictionaryOfUpdatedProducts[int(index)])
                 print(">--------------------------------------------<")
-                print("Total amount in the cart : ",totalinCart)
+                print("Total amount in the cart : $%i"%totalinCart)
                 itemsInTheCart.append(dictionaryOfUpdatedProducts[int(index)])
                 print("Items in the cart : ",itemsInTheCart)
                 print(">--------------------------------------------<")            
             else:
-                print(">===============< Cart empty. Checking Out >===============<")
-                break
+                print(">===============< Cart empty >===============<")
+                continue
         #Exception to catch if the user input is incorrect    
         except Exception:
             print("\nXXXXXXXXXXXXXXXXXXXXXXXX No such item for that index in the list XXXXXXXXXXXXXXXXXXXXXXXXXXX\n")
